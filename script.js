6b880d906d6f3443d104ebc17b00119f63bf3be6
sabrigs <sabrinagsilva@proton.me>
2026-08-13 16:24:55 -0300

    Refactor wallpaper handling in index.html and script.js for improved image management and remove unnecessary styles from style.css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const wallpaper = window.document.querySelector("main#wallpaper");
+const wallpaper = window.document.querySelector("img#wallpaper");
 const title = window.document.querySelector("h1#title");
 const caption = window.document.querySelector("p#caption");
 const colorBackground = window.document.querySelector("body");
@@ -14,25 +14,25 @@ function updateClock() {
 function updateTheme() {
     if (currentHour >= 0 && currentHour <= 4) {
         caption.innerHTML = "Good evening, sleeper!";
-        wallpaper.style.backgroundImage = "url('img/night.jpeg')";
+        wallpaper.src = "img/night.jpeg";
         colorBackground.style.backgroundColor = "#19241E";
     }
 
     else if (currentHour >= 5 && currentHour <= 11) {
         caption.innerHTML = "Good morning!";
-        wallpaper.style.backgroundImage = "url('img/morning.jpeg')";
+        wallpaper.src = "img/morning.jpeg";
         colorBackground.style.backgroundColor = "#E4CA91";
     } 
 
     else if (currentHour >= 12 && currentHour <= 18) {
         caption.innerHTML = "Good afternoon!"
-        wallpaper.style.backgroundImage = "url('img/afternoon.jpeg')";
+        wallpaper.src = "img/afternoon.jpeg";
         colorBackground.style.backgroundColor = "#E3C797";
     } 
 
     else if (currentHour >= 19 && currentHour <= 23) {
         caption.innerHTML = "Good evening!"
-        wallpaper.style.backgroundImage = "url('img/night.jpeg')";
+        wallpaper.src = "img/night.jpeg";
         colorBackground.style.backgroundColor = "#19241E";
     }   
 }
